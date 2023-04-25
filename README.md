7c91381c28b976521e1fdb8f598c4143607f2b13
strunk23 <225820@student.uek.krakow.pl>
2023-04-25 17:59:43 +0200

    2023-04-25 MG

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # CeneoScraper
+
+### CSS selectors of the components of all opinions in [Ceneo.pl](https://www.ceneo.pl/) service
+
+|Components|Variable/Dictionary key|Data type|Selector|
+| :- | :- | :- | :- |
+|opinion|opinion/single\_opinion|Tag, dictionary|div.js\_product-review|
+|opinion ID|opinion\_id|string|["data-entry-id"]|
+|opinion’s author|author|string|span.user-post\_\_author-name|
+|author’s recommendation|recommendation|bool|span.user-post\_\_author-recomendation > em|
+|score expressed in number of stars|score|float|span.user-post\_\_score-count|
+|opinion’s content|description|string|div.user-post\_\_text|
+|list of product advantages|pros|string|div.review-feature\_\_col:has( > div.review-feature\_\_title--positives) > div.review-feature\_\_item|
+|list of product disadvantages|cons|string|div.review-feature\_\_col:has( > div.review-feature\_\_title--negatives) > div.review-feature\_\_item|
+|how many users think that opinion was helpful|like|int|<p>button.vote-yes["data-total-vote"]</p><p>button.vote-yes > span</p><p>span[id^=votes-yes]</p>|
+|how many users think that opinion was unhelpful|dislike|int|<p>button.vote-no["data-total-vote"]</p><p>button.vote-no > span</p><p>span[id^=votes-no]</p>|
+|publishing date|publish\_date|string|span.user-post\_\_published > time:nth-child(1) ["datetime"]|
+|purchase date|purchase\_date|string|span.user-post\_\_published > time:nth-child(2) ["datetime"]|
+
+## Python libraries used in project
+1. Requests
+2. BeautifulSoup
+3. Json
+4. Os
+5. Translate
+6. Numpy
--- scraper.py
+++ scraper.py
@@ -1,11 +1,15 @@
-import json
 import os
+import json
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
+from translate import Translator
 
 
-def get_element(dom_tree, selector=None, attribute=None):
+def get_element(dom_tree, selector=None, attribute=None, return_list=False):
     try:
+        if return_list:
+            return ", ".join(tag.text.strip() for tag in dom_tree.select(selector))
         if attribute:
             if selector:
                 return dom_tree.select_one(selector)[attribute].text.strip()
@@ -14,58 +18,65 @@ def get_element(dom_tree, selector=None, attribute=None):
     
     except (AttributeError, TypeError):
         return None
+    
+def clean_text(text):
+    return ' '.join(text.replace(r"\s", " ").split())
 
-product_code = input("Please enter the product code: ")
-# product_code = "129901214"
+selectors = {
+    "opinion_id": [None, "data-entry-id"],
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recommendation > em"],
+    "score": ["span.user-post__score-count"],
+    "description": ["div.user-post__text"],
+    "pros": ["div.review-feature__col:has( > div.review-feature__title--positives) > div.review-feature__item", None, True],
+    "cons": ["div.review-feature__col:has( > div.review-feature__title--negatives) > div.review-feature__item", None, True],
+    "like": ["button.vote-yes > span"],
+    "dislike": ["button.vote-yes > span"],
+    "publish_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"]
+    }
+
+
+#product_code = input("Please enter the product code: ")
+product_code = "129901214"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 all_opinions = []
+
+from_lang = "pl"
+to_land = "en"
+translator = Translator(to_land, from_lang)
 
 while url:
     response = requests.get(url)
 
     if response.status_code == requests.codes.ok:
         page_done = BeautifulSoup(response.text, 'html.parser')
-        print(get_element(page_done))
         opinions = page_done.select("div.js_product-review")
         
         if len(opinions) > 0:
 
             for opinion in opinions:
+                
+                single_opinion = {}
 
-                opinion_id = opinion["data-entry-id"]
-                author = get_element(opinion, "span.user-post__author-name")
-                recommendation = get_element(opinion, "span.user-post__author-recommendation > em")
-                score = get_element(opinion, "span.user-post__score-count")
-                description = get_element(opinion, "div.user-post__text")
-                pros = opinion.select("div.review-feature__col:has( > div.review-feature__title--positives) > div.review-feature__item")
-                pros = [p.text.strip() for p in pros]
-                cons = opinion.select("div.review-feature__col:has( > div.review-feature__title--negatives) > div.review-feature__item")
-                cons = [c.text.strip() for c in cons]
-                like = get_element(opinion, "button.vote-yes > span")
-                dislike = get_element(opinion, "button.vote-yes > span")
-                publish_date = get_element(opinion, "span.user-post__published > time:nth-child(1)", "datetime")
-                purchase_date = get_element(opinion, "span.user-post__published > time:nth-child(2)", "datetime")
+                for key, value in selectors.items():
+                    single_opinion[key] = get_element(opinion, *value)
 
-                single_opinion = {
-                    "opinion_id": opinion_id,
-                    "author": author,
-                    "recommendation": recommendation,
-                    "score": score,
-                    "description": description,
-                    "pros": pros,
-                    "cons": cons,
-                    "like": like,
-                    "dislike": dislike,
-                    "publish_date": publish_date,
-                    "purchase_date": purchase_date
-                }
+                single_opinion["recommendation"] = True if single_opinion["recommendation"] == "Polecam" else False if single_opinion["recommendation"] == "Nie polecam" else None
+                single_opinion["score"] = np.divide(*[float(score.replace(",", ".")) for score in single_opinion["score"].split("/")])
+                single_opinion["like"] = int(single_opinion["like"])
+                single_opinion["dislike"] = int(single_opinion["dislike"])
+                single_opinion["description"] = clean_text(single_opinion["description"])
+                single_opinion["description_en"] = translator.translate(single_opinion["description"][:500])
+                single_opinion["pros_en"] = translator.translate(single_opinion["pros"])
+                single_opinion["cons_en"] = translator.translate(single_opinion["cons"])
 
                 all_opinions.append(single_opinion)
+
             try:
                 url = "https://www.ceneo.pl" + get_element(page_done, "a.pagination__next", "href")
             except TypeError:
                 url = None
-            print(url)
 
         else:
             print(f"There are no opinions about product with code {product_code}")
